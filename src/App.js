2feccee5cee3f891a9d51b5f1377a9279f162ef0
Agnes Ostina <ladisedna02@gmail.com>
2026-02-25 14:45:30 +0530

    [SCRUM-15] Added parent progress page inside progressMonitoring Module

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // src/App.js
 import React from 'react';
 import ParentProgressPage from './features/progressMonitoring/ParentProgressPage';
-// Import New  Dashboard
-import ActivityDashboard from './features/activityManagement/ActivityDashboard';
+
 
 function App() {
   return (
     <div className="App">
-      {/* The command for view in Dashboard  */}
-      <ActivityDashboard />
       
       {/* Parent Progress page */}
       <ParentProgressPage />
